3cbb7d78dcb9e7e4d60c0d32a3ab75f87dea85c2
luisislabao <luisislabao@gmail.com>
2023-09-05 23:09:30 -0300

    mesma coisa

--- src/components/get-products.vue
+++ src/components/get-products.vue
@@ -3,10 +3,10 @@
 
 
 const newProducts = [
-    { "id": 1, "name": "Bolo de Chocolate", "Quantitie": 10, "price": 120.51, "imgID": "", "categories": [{ "id": 1, "name": "Bolos" }] },
-    { "id": 2, "name": "Brownie", "Quantitie": 10, "price": 45.5, "imgID": "", "categories": [{ "id": 2, "name": "Bolos" }] },
-    { "id": 3, "name": "Brigadeiro", "Quantitie": 10, "price": 45.5, "imgID": "", "categories": [{ "id": 2, "name": "Doces" }] },
-    { "id": 4, "name": "Quindim", "Quantitie": 10, "price": 45.5, "imgID": "", "categories": [{ "id": 2, "name": "Doces" }] },
+    { "id": 1, "name": "Bolo de Chocolate", "Quantitie": 10, "price": 120.51, "imgID": "", "categories": [{ "id": 3, "name": "Bolos" }] },
+    { "id": 2, "name": "Brownie", "Quantitie": 10, "price": 45.5, "imgID": "", "categories": [{ "id": 3 , "name": "Bolos" }] },
+    { "id": 3, "name": "Brigadeiro", "Quantitie": 10, "price": 45.5, "imgID": "", "categories": [{ "id": 1, "name": "Doces" }] },
+    { "id": 4, "name": "Quindim", "Quantitie": 10, "price": 45.5, "imgID": "", "categories": [{ "id": 1, "name": "Doces" }] },
     { "id": 4, "name": "Coca 250ml", "Quantitie": 10, "price": 45.5, "imgID": "", "categories": [{ "id": 2, "name": "Bebidas" }] },
     { "id": 4, "name": "Pepsi 250ml", "Quantitie": 10, "price": 45.5, "imgID": "", "categories": [{ "id": 2, "name": "Bebidas" }] },
     { "id": 4, "name": "Biri 250ml", "Quantitie": 10, "price": 45.5, "imgID": "", "categories": [{ "id": 2, "name": "Bebidas" }] },
