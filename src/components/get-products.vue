191ab01f30797e1d3fc98fd0538cc0b56b0a0de7
luisislabao <luisislabao@gmail.com>
2023-09-05 23:04:45 -0300

    só formatei a estrutura

--- src/components/get-products.vue
+++ src/components/get-products.vue
@@ -2,20 +2,22 @@
 
 
 
-const newProducts = [{"id":1,"name":"Bolo de Chocolate","Quantitie":10,"price":120.51,"imgID":"","categories":[{"id":1,"name":"Bolos"}]},
-                    {"id":2,"name":"Brownie","Quantitie":10,"price":45.5,"imgID":"","categories":[{"id":2,"name":"Bolos"}]},
-                    {"id":3,"name":"Brigadeiro","Quantitie":10,"price":45.5,"imgID":"","categories":[{"id":2,"name":"Doces"}]},
-                    {"id":4,"name":"Quindim","Quantitie":10,"price":45.5,"imgID":"","categories":[{"id":2,"name":"Doces"}]},
-                    {"id":4,"name":"Coca 250ml","Quantitie":10,"price":45.5,"imgID":"","categories":[{"id":2,"name":"Bebidas"}]},
-                    {"id":4,"name":"Pepsi 250ml","Quantitie":10,"price":45.5,"imgID":"","categories":[{"id":2,"name":"Bebidas"}]},
-                    {"id":4,"name":"Biri 250ml","Quantitie":10,"price":45.5,"imgID":"","categories":[{"id":2,"name":"Bebidas"}]},
-                    {"id":4,"name":"Fanta 250ml","Quantitie":10,"price":45.5,"imgID":"","categories":[{"id":2,"name":"Bebidas"}]},
-                    {"id":4,"name":"Dolly 250ml","Quantitie":10,"price":45.5,"imgID":"","categories":[{"id":2,"name":"Bebidas"}]}]
-export {newProducts}
-export default{
+const newProducts = [
+    { "id": 1, "name": "Bolo de Chocolate", "Quantitie": 10, "price": 120.51, "imgID": "", "categories": [{ "id": 1, "name": "Bolos" }] },
+    { "id": 2, "name": "Brownie", "Quantitie": 10, "price": 45.5, "imgID": "", "categories": [{ "id": 2, "name": "Bolos" }] },
+    { "id": 3, "name": "Brigadeiro", "Quantitie": 10, "price": 45.5, "imgID": "", "categories": [{ "id": 2, "name": "Doces" }] },
+    { "id": 4, "name": "Quindim", "Quantitie": 10, "price": 45.5, "imgID": "", "categories": [{ "id": 2, "name": "Doces" }] },
+    { "id": 4, "name": "Coca 250ml", "Quantitie": 10, "price": 45.5, "imgID": "", "categories": [{ "id": 2, "name": "Bebidas" }] },
+    { "id": 4, "name": "Pepsi 250ml", "Quantitie": 10, "price": 45.5, "imgID": "", "categories": [{ "id": 2, "name": "Bebidas" }] },
+    { "id": 4, "name": "Biri 250ml", "Quantitie": 10, "price": 45.5, "imgID": "", "categories": [{ "id": 2, "name": "Bebidas" }] },
+    { "id": 4, "name": "Fanta 250ml", "Quantitie": 10, "price": 45.5, "imgID": "", "categories": [{ "id": 2, "name": "Bebidas" }] },
+    { "id": 4, "name": "Dolly 250ml", "Quantitie": 10, "price": 45.5, "imgID": "", "categories": [{ "id": 2, "name": "Bebidas" }] }
+]
+export { newProducts }
+export default {
     name: "GetProducts",
-    data (){
-        return{
+    data() {
+        return {
             newProducts
         }
     }
